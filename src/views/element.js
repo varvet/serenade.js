da2841ea4f8549a98a3e38cf67bb82937df71e31
Jonas Nicklas <jonas.nicklas@gmail.com>
2015-03-14 19:26:19 +0100

    More ES6

--- src/views/element.js
+++ src/views/element.js
@@ -1,225 +1,209 @@
 import View from "./view"
 import defineEvent from "../event"
-import { def, assignUnlessEqual } from "../helpers"
+import { settings, assignUnlessEqual } from "../helpers"
 import Compile from "../compile"
 import Collection from "../collection"
 
-var Element,
-  __hasProp = {}.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
-
-Element = (function(_super) {
-  __extends(Element, _super);
-
-  defineEvent(Element.prototype, "load", {
-    async: false
-  });
-
-  defineEvent(Element.prototype, "unload", {
-    async: false
-  });
-
-  function Element(ast, context) {
-    var child, childView, _i, _len, _ref, _ref1;
-    this.ast = ast;
-    this.context = context;
-    Element.__super__.constructor.call(this, Serenade.document.createElement(this.ast.name));
-    if (this.ast.id) {
-      this.node.setAttribute('id', this.ast.id);
+const Property = {
+  style: {
+    update: function(property, value) {
+      assignUnlessEqual(this.node.style, property.name, value);
     }
-    if ((_ref = this.ast.classes) != null ? _ref.length : void 0) {
-      this.node.setAttribute('class', this.ast.classes.join(' '));
+  },
+  event: {
+    setup: function(property) {
+      this.node.addEventListener(property.name, (e) => {
+        if(property.preventDefault) {
+          e.preventDefault();
+        }
+        this.context[property.value](this.node, this.context, e);
+      });
     }
-    _ref1 = this.ast.children;
-    for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
-      child = _ref1[_i];
-      childView = Compile[child.type](child, this.context);
-      childView.append(this.node);
-      this.children.push(childView);
+  },
+  class: {
+    update: function(property, value) {
+      if(value) {
+        if(!this.boundClasses) {
+          this.boundClasses = new Collection();
+        }
+        if(!this.boundClasses.includes(property.name)) {
+          this.boundClasses.push(property.name);
+          this._updateClass();
+        }
+      } else if(this.boundClasses) {
+        let index = this.boundClasses.indexOf(property.name);
+        this.boundClasses.delete(property.name);
+        this._updateClass();
+      }
     }
-    this.ast.properties.forEach((function(_this) {
-      return function(property) {
-        var action;
-        action = property.scope === "attribute" && property.name === "binding" ? _this.property.binding : _this.property[property.scope];
-        if (action) {
-          if (action.setup) {
-            action.setup.call(_this, property);
-          }
-          if (action.update) {
-            if (property["static"]) {
-              return action.update.call(_this, property, _this.context[property.value]);
-            } else if (property.bound) {
-              if (property.value) {
-                return _this._bindToModel(property.value, function(value) {
-                  return action.update.call(_this, property, value);
-                });
-              } else {
-                return action.update.call(_this, property, _this.context);
-              }
-            } else {
-              return action.update.call(_this, property, property.value);
-            }
-          } else if (property.bound) {
-            throw SyntaxError("properties in scope " + property.scope + " cannot be bound, use: `" + property.scope + ":" + property.name + "=" + property.value + "`");
+  },
+  binding: {
+    setup: function(property) {
+      if(this.ast.name !== "input" && this.ast.name !== "textarea" && this.ast.name !== "select") {
+        throw SyntaxError("invalid view type " + this.ast.name + " for two way binding");
+      }
+      if(!property.value) {
+        throw SyntaxError("cannot bind to whole context, please specify an attribute to bind to");
+      }
+      let domUpdated = () => {
+        if(this.node.type === "checkbox") {
+          this.context[property.value] = this.node.checked;
+        } else if(this.node.type === "radio") {
+          if(this.node.checked) {
+            this.context[property.value] = this.node.value;
           }
         } else {
-          throw SyntaxError("" + property.scope + " is not a valid scope");
+          this.context[property.value] = this.node.value;
         }
-      };
-    })(this));
+      }
+      if(property.name === "binding") {
+        let handler = (e) => {
+          if (this.node.form === (e.target || e.srcElement)) {
+            domUpdated();
+          }
+        };
+        settings.document.addEventListener("submit", handler, true);
+        this.unload.bind(function() {
+          settings.document.removeEventListener("submit", handler, true);
+        });
+      } else {
+        this.node.addEventListener(property.name, domUpdated);
+      }
+    },
+    update: function(property, value) {
+      if(this.node.type === "checkbox") {
+        return this.node.checked = !!value;
+      } else if(this.node.type === "radio") {
+        if(value === this.node.value) {
+          return this.node.checked = true;
+        }
+      } else {
+        if(value === undefined) {
+          value = "";
+        }
+        return assignUnlessEqual(this.node, "value", value);
+      }
+    }
+  },
+  attribute: {
+    update: function(property, value) {
+      if(property.name === 'value') {
+        assignUnlessEqual(this.node, "value", value || '');
+      } else if(this.ast.name === 'input' && property.name === 'checked') {
+        assignUnlessEqual(this.node, "checked", !!value);
+      } else if(property.name === 'class') {
+        this.attributeClasses = value;
+        this._updateClass();
+      } else if(value === void 0) {
+        if(this.node.hasAttribute(property.name)) {
+          this.node.removeAttribute(property.name);
+        }
+      } else {
+        if(value === 0) {
+          value = "0";
+        }
+        if(this.node.getAttribute(property.name) !== value) {
+          this.node.setAttribute(property.name, value);
+        }
+      }
+    }
+  },
+  on: {
+    setup: function(property) {
+      if(property.name === "load" || property.name === "unload") {
+        this[property.name].bind(function() {
+          this.context[property.value](this.node, this.context);
+        });
+      } else {
+        throw new SyntaxError("unkown lifecycle event '" + property.name + "'");
+      }
+    }
+  },
+  property: {
+    update: function(property, value) {
+      assignUnlessEqual(this.node, property.name, value);
+    }
+  }
+};
+
+class Element extends View {
+  constructor(ast, context) {
+    this.ast = ast;
+    this.context = context;
+
+    super(settings.document.createElement(ast.name))
+
+    if (ast.id) {
+      this.node.setAttribute('id', ast.id);
+    }
+
+    if(ast.classes && ast.classes.length) {
+      this.node.setAttribute('class', ast.classes.join(' '));
+    }
+
+    ast.children.forEach((child) => {
+      let childView = Compile[child.type](child, context);
+      childView.append(this.node);
+      this.children.push(childView);
+    });
+
+    ast.properties.forEach((property) => {
+      let action;
+      if(property.scope === "attribute" && property.name === "binding") {
+        action = Property.binding
+      } else {
+        action = Property[property.scope];
+      }
+      if(action) {
+        if(action.setup) {
+          action.setup.call(this, property);
+        }
+        if(action.update) {
+          if(property["static"]) {
+            action.update.call(this, property, this.context[property.value]);
+          } else if (property.bound) {
+            if (property.value) {
+              this._bindToModel(property.value, (value) => action.update.call(this, property, value));
+            } else {
+              action.update.call(this, property, this.context);
+            }
+          } else {
+            action.update.call(this, property, property.value);
+          }
+        } else if(property.bound) {
+          throw SyntaxError("properties in scope " + property.scope + " cannot be bound, use: `" + property.scope + ":" + property.name + "=" + property.value + "`");
+        }
+      } else {
+        throw SyntaxError("" + property.scope + " is not a valid scope");
+      }
+    });
     this.load.trigger();
   }
 
-  Element.prototype._updateClass = function() {
-    var classes, _ref;
-    classes = this.ast.classes;
-    if (this.attributeClasses) {
+  _updateClass() {
+    let classes = this.ast.classes;
+    if(this.attributeClasses) {
       classes = classes.concat(this.attributeClasses);
     }
-    if ((_ref = this.boundClasses) != null ? _ref.length : void 0) {
+    if(this.boundClasses && this.boundClasses.length) {
       classes = classes.concat(this.boundClasses.toArray());
     }
-    classes.sort();
-    if (classes.length) {
-      return assignUnlessEqual(this.node, "className", classes.join(' '));
+    if(classes.length) {
+      assignUnlessEqual(this.node, "className", classes.sort().join(' '));
     } else {
-      return this.node.removeAttribute("class");
+      this.node.removeAttribute("class");
     }
   };
 
-  Element.prototype.detach = function() {
+  detach() {
     this.unload.trigger();
-    return Element.__super__.detach.apply(this, arguments);
+    super.detach();
   };
 
-  Element.prototype.property = {
-    style: {
-      update: function(property, value) {
-        return assignUnlessEqual(this.node.style, property.name, value);
-      }
-    },
-    event: {
-      setup: function(property) {
-        return this.node.addEventListener(property.name, (function(_this) {
-          return function(e) {
-            if (property.preventDefault) {
-              e.preventDefault();
-            }
-            return _this.context[property.value](_this.node, _this.context, e);
-          };
-        })(this));
-      }
-    },
-    "class": {
-      update: function(property, value) {
-        var index, _ref;
-        if (value) {
-          this.boundClasses || (this.boundClasses = new Collection());
-          if (!((_ref = this.boundClasses) != null ? _ref.includes(property.name) : void 0)) {
-            this.boundClasses.push(property.name);
-            return this._updateClass();
-          }
-        } else if (this.boundClasses) {
-          index = this.boundClasses.indexOf(property.name);
-          this.boundClasses["delete"](property.name);
-          return this._updateClass();
-        }
-      }
-    },
-    binding: {
-      setup: function(property) {
-        var domUpdated, handler, _ref;
-        ((_ref = this.ast.name) === "input" || _ref === "textarea" || _ref === "select") || (function() {
-          throw SyntaxError("invalid view type " + this.ast.name + " for two way binding");
-        }).call(this);
-        property.value || (function() {
-          throw SyntaxError("cannot bind to whole context, please specify an attribute to bind to");
-        })();
-        domUpdated = function() {
-          if(this.node.type === "checkbox") {
-            this.context[property.value] = this.node.checked;
-          } else if(this.node.type === "radio") {
-            if(this.node.checked) {
-              this.context[property.value] = this.node.value;
-            }
-          } else {
-            this.context[property.value] = this.node.value;
-          }
-        }.bind(this);
-        if (property.name === "binding") {
-          handler = function(e) {
-            if (this.node.form === (e.target || e.srcElement)) {
-              return domUpdated();
-            }
-          }.bind(this);
-          Serenade.document.addEventListener("submit", handler, true);
-          return this.unload.bind(function() {
-            return Serenade.document.removeEventListener("submit", handler, true);
-          });
-        } else {
-          return this.node.addEventListener(property.name, domUpdated);
-        }
-      },
-      update: function(property, value) {
-        if (this.node.type === "checkbox") {
-          return this.node.checked = !!value;
-        } else if (this.node.type === "radio") {
-          if (value === this.node.value) {
-            return this.node.checked = true;
-          }
-        } else {
-          if (value === void 0) {
-            value = "";
-          }
-          return assignUnlessEqual(this.node, "value", value);
-        }
-      }
-    },
-    attribute: {
-      update: function(property, value) {
-        if (property.name === 'value') {
-          return assignUnlessEqual(this.node, "value", value || '');
-        } else if (this.ast.name === 'input' && property.name === 'checked') {
-          return assignUnlessEqual(this.node, "checked", !!value);
-        } else if (property.name === 'class') {
-          this.attributeClasses = value;
-          return this._updateClass();
-        } else if (value === void 0) {
-          if (this.node.hasAttribute(property.name)) {
-            return this.node.removeAttribute(property.name);
-          }
-        } else {
-          if (value === 0) {
-            value = "0";
-          }
-          if (this.node.getAttribute(property.name) !== value) {
-            return this.node.setAttribute(property.name, value);
-          }
-        }
-      }
-    },
-    on: {
-      setup: function(property) {
-        var _ref;
-        if ((_ref = property.name) === "load" || _ref === "unload") {
-          return this[property.name].bind(function() {
-            return this.context[property.value](this.node, this.context);
-          });
-        } else {
-          throw new SyntaxError("unkown lifecycle event '" + property.name + "'");
-        }
-      }
-    },
-    property: {
-      update: function(property, value) {
-        return assignUnlessEqual(this.node, property.name, value);
-      }
-    }
-  };
+}
 
-  return Element;
-
-})(View);
+defineEvent(Element.prototype, "load", { async: false });
+defineEvent(Element.prototype, "unload", { async: false });
 
 Compile.element = function(ast, context) {
   return Serenade.renderView(ast, context);
