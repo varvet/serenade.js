e292dafcf6f968ece2aaf1490a30d6ae59be6c51
Jonas Nicklas <jonas.nicklas@gmail.com>
2015-04-01 09:51:50 +0200

    Finish up Lexer

--- src/lexer.js
+++ src/lexer.js
@@ -1,9 +1,6 @@
-var COMMENT, IDENTIFIER, KEYWORDS, LITERALS, Lexer, MULTI_DENT, STRING, WHITESPACE,
-	__indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+const IDENTIFIER = /^[a-zA-Z][a-zA-Z0-9\-_]*/;
 
-IDENTIFIER = /^[a-zA-Z][a-zA-Z0-9\-_]*/;
-
-LITERALS = {
+const LITERALS = {
 	"[": "LBRACKET",
 	"]": "RBRACKET",
 	"=": "EQUALS",
@@ -15,22 +12,20 @@ LITERALS = {
 	"@": "AT"
 }
 
-STRING = /^"((?:\\.|[^"])*)"/;
+const STRING = /^"((?:\\.|[^"])*)"/;
 
-MULTI_DENT = /^(?:\r?\n[^\r\n\S]*)+/;
+const MULTI_DENT = /^(?:\r?\n[^\r\n\S]*)+/;
 
-WHITESPACE = /^[^\r\n\S]+/;
+const WHITESPACE = /^[^\r\n\S]+/;
 
-COMMENT = /^\s*\/\/[^\n]*/;
+const COMMENT = /^\s*\/\/[^\n]*/;
 
-KEYWORDS = ["IF", "ELSE", "COLLECTION", "IN", "VIEW", "UNLESS"];
+const KEYWORDS = ["IF", "ELSE", "COLLECTION", "IN", "VIEW", "UNLESS"];
 
-Lexer = (function() {
-	function Lexer() {}
-
-	Lexer.prototype.tokenize = function(code, opts) {
+class Lexer {
+	tokenize(code, opts) {
 		var tag;
-		if (opts == null) {
+		if(opts == null) {
 			opts = {};
 		}
 		this.code = code.replace(/^\s*/, '').replace(/\s*$/, '');
@@ -40,56 +35,56 @@ Lexer = (function() {
 		this.ends = [];
 		this.tokens = [];
 		this.i = 0;
-		while (this.chunk = this.code.slice(this.i)) {
+		while(this.chunk = this.code.slice(this.i)) {
 			this.i += this.identifierToken() || this.commentToken() || this.whitespaceToken() || this.lineToken() || this.stringToken() || this.literalToken();
 		}
-		while (tag = this.ends.pop()) {
-			if (tag === 'OUTDENT') {
+		while(tag = this.ends.pop()) {
+			if(tag === 'OUTDENT') {
 				this.token('OUTDENT');
 			} else {
 				this.error("missing " + tag);
 			}
 		}
-		while (this.tokens[0][0] === "TERMINATOR") {
+		while(this.tokens[0][0] === "TERMINATOR") {
 			this.tokens.shift();
 		}
-		while (this.tokens[this.tokens.length - 1][0] === "TERMINATOR") {
+		while(this.tokens[this.tokens.length - 1][0] === "TERMINATOR") {
 			this.tokens.pop();
 		}
 		return this.tokens;
-	};
+	}
 
-	Lexer.prototype.commentToken = function() {
-		var match;
-		if (match = COMMENT.exec(this.chunk)) {
+	commentToken() {
+		let match = COMMENT.exec(this.chunk);
+		if(match) {
 			return match[0].length;
 		} else {
 			return 0;
 		}
-	};
+	}
 
-	Lexer.prototype.whitespaceToken = function() {
-		var match;
-		if (match = WHITESPACE.exec(this.chunk)) {
+	whitespaceToken() {
+		let match = WHITESPACE.exec(this.chunk);
+		if(match) {
 			this.token('WHITESPACE', match[0].length);
 			return match[0].length;
 		} else {
 			return 0;
 		}
-	};
+	}
 
-	Lexer.prototype.token = function(tag, value) {
+	token(tag, value) {
 		return this.tokens.push([tag, value, this.line]);
-	};
+	}
 
-	Lexer.prototype.identifierToken = function() {
-		var match, name;
-		if (match = IDENTIFIER.exec(this.chunk)) {
-			name = match[0].toUpperCase();
+	identifierToken() {
+    let match = IDENTIFIER.exec(this.chunk);
+		if(match) {
+			let name = match[0].toUpperCase();
 			if (name === "ELSE" && this.last(this.tokens, 2)[0] === "TERMINATOR") {
 				this.tokens.splice(this.tokens.length - 3, 1);
 			}
-			if (__indexOf.call(KEYWORDS, name) >= 0) {
+			if (KEYWORDS.indexOf(name) >= 0) {
 				this.token(name, match[0]);
 			} else {
 				this.token('IDENTIFIER', match[0]);
@@ -98,28 +93,31 @@ Lexer = (function() {
 		} else {
 			return 0;
 		}
-	};
+	}
 
-	Lexer.prototype.stringToken = function() {
-		var match;
-		if (match = STRING.exec(this.chunk)) {
+	stringToken() {
+    let match = STRING.exec(this.chunk);
+		if(match) {
 			this.token('STRING_LITERAL', match[1].replace(/\\(.)/g, "$1"));
 			return match[0].length;
 		} else {
 			return 0;
 		}
-	};
+	}
 
-	Lexer.prototype.lineToken = function() {
-		var diff, indent, match, prev, size;
-		if (!(match = MULTI_DENT.exec(this.chunk))) {
-			return 0;
-		}
-		indent = match[0];
+	lineToken() {
+    let match = MULTI_DENT.exec(this.chunk);
+
+		if(!match) { return 0; }
+
+		let indent = match[0];
+
 		this.line += this.count(indent, '\n');
-		prev = this.last(this.tokens, 1);
-		size = indent.length - 1 - indent.lastIndexOf('\n');
-		diff = size - this.indent;
+
+		let prev = this.last(this.tokens, 1);
+		let size = indent.length - 1 - indent.lastIndexOf('\n');
+		let diff = size - this.indent;
+
 		if (size === this.indent) {
 			this.newlineToken();
 		} else if (size > this.indent) {
@@ -136,43 +134,43 @@ Lexer = (function() {
 		}
 		this.indent = size;
 		return indent.length;
-	};
+	}
 
-	Lexer.prototype.literalToken = function() {
-		var match;
-		if (match = LITERALS[this.chunk[0]]) {
+	literalToken() {
+		let match = LITERALS[this.chunk[0]];
+		if(match) {
 			this.token(match);
 			return 1;
 		} else {
 			return this.error("Unexpected token '" + (this.chunk.charAt(0)) + "'");
 		}
-	};
+	}
 
-	Lexer.prototype.newlineToken = function() {
-		if (this.tag() !== 'TERMINATOR') {
+	newlineToken() {
+		if(this.tag() !== 'TERMINATOR') {
 			return this.token('TERMINATOR', '\n');
 		}
-	};
+	}
 
-	Lexer.prototype.tag = function(index, tag) {
+	tag(index, tag) {
 		var tok;
 		return (tok = this.last(this.tokens, index)) && (tag ? tok[0] = tag : tok[0]);
-	};
+	}
 
-	Lexer.prototype.value = function(index, val) {
+	value(index, val) {
 		var tok;
 		return (tok = this.last(this.tokens, index)) && (val ? tok[1] = val : tok[1]);
-	};
+	}
 
-	Lexer.prototype.error = function(message) {
+	error(message) {
 		var chunk;
 		chunk = this.code.slice(Math.max(0, this.i - 10), Math.min(this.code.length, this.i + 10));
 		throw SyntaxError("" + message + " on line " + (this.line + 1) + " near " + (JSON.stringify(chunk)));
-	};
+	}
 
-	Lexer.prototype.count = function(string, substr) {
-		var num, pos;
-		num = pos = 0;
+	count(string, substr) {
+    let num = 0;
+    let pos = 0;
 		if (!substr.length) {
 			return 1 / 0;
 		}
@@ -180,14 +178,12 @@ Lexer = (function() {
 			num++;
 		}
 		return num;
-	};
+	}
 
-	Lexer.prototype.last = function(array, back) {
+	last(array, back) {
 		return array[array.length - (back || 0) - 1];
-	};
+	}
 
-	return Lexer;
-
-})();
+}
 
 export default Lexer;
