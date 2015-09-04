e27e11ed35e95e09c48aab2a3cb2fc381067601b
Jonas Nicklas <jonas.nicklas@gmail.com>
2015-09-04 20:10:45 +0200

    Fix bounds views

--- src/views/bound_view_view.js
+++ src/views/bound_view_view.js
@@ -5,18 +5,23 @@ import { settings } from "../helpers"
 class BoundViewView extends DynamicView {
   constructor(ast, context) {
     super(ast, context);
-    this._bindToModel(ast.argument, (value) => {
-        let view = settings.templates[value].render(context).view;
-        this.replace([view]);
+
+    Compile.parameter(ast.arguments[0], context).bind((value) => {
+      let view = settings.templates[value].render(context).view;
+      this.replace([view]);
     });
   }
 }
 
 Compile.view = function(ast, context) {
-  if (ast.bound) {
+  if(ast.arguments.length !== 1) {
+    throw(new Error("`if` must take exactly one argument"))
+  }
+
+  if (ast.arguments[0].bound) {
     return new BoundViewView(ast, context);
   } else {
-    return settings.templates[ast.argument].render(context).view;
+    return settings.templates[ast.arguments[0].value].render(context).view;
   }
 };
 
